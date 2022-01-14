37bb0080b2e589dcf281a41b535f094ab7162464
blkadriu <56959910+blkadriu@users.noreply.github.com>
2022-01-14 20:08:48 +0100

    Final push

--- src/SymCryptoMain.java
+++ src/SymCryptoMain.java
@@ -11,9 +11,7 @@ public class SymCryptoMain {
 	// Read "files" directory and ask user to encrypt/decrypt all files
 	public static void main(String[] args) {
 
-		// Get list of files in the directory
-		File dir = new File("files");
-		File[] filelist = dir.listFiles();
+		//Initialize the JFrame and the fileChooser
 		final JFrame frame = new JFrame("CRYPTOJAVA ENCRYPTION");
 		final JFileChooser choose = new JFileChooser();
 
@@ -26,20 +24,22 @@ public class SymCryptoMain {
 			btn1.addActionListener(new ActionListener() {
 
 				public void actionPerformed(ActionEvent e) {
-					int value = choose.showDialog(frame, "Encrypt");
+					int response = choose.showDialog(frame, "Encrypt");
 					File file = choose.getSelectedFile();
-					if (value == JFileChooser.APPROVE_OPTION) {
+					if (response == JFileChooser.APPROVE_OPTION) {
 						try {
 							CryptoMethods.encryptFile(file, keygen.getCipher(), keygen.getSecretKey());
 						} catch (Exception exeption) {
 							JOptionPane.showMessageDialog(frame, file.getName() + " File encryption has failed",
 									"Encryption Unsucessfull", JOptionPane.ERROR_MESSAGE);
-
 						}
+
+						JOptionPane.showMessageDialog(frame, file.getName() + " Has Been Encrypted", "Encryption Sucessfull",
+								JOptionPane.INFORMATION_MESSAGE, new ImageIcon("src/check.png"));
+					} else if (response == JFileChooser.CANCEL_OPTION) {
+						JOptionPane.showMessageDialog(frame, "Canceled Encryption", "Cancel",
+								JOptionPane.INFORMATION_MESSAGE);
 					}
-					;
-					JOptionPane.showMessageDialog(frame, "File encryption finished", "Encryption Sucessfull",
-							JOptionPane.INFORMATION_MESSAGE);
 
 				}
 			});
@@ -47,9 +47,9 @@ public class SymCryptoMain {
 			JButton btn2 = new JButton("Decrypt a file");
 			btn2.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					int retVal = choose.showDialog(frame, "Decrypt");
+					int response = choose.showDialog(frame, "Decrypt");
 					File file = choose.getSelectedFile();
-					if (retVal == JFileChooser.APPROVE_OPTION) {
+					if (response == JFileChooser.APPROVE_OPTION) {
 						try {
 							CryptoMethods.decryptFile(file, keygen.getCipher(), keygen.getSecretKey());
 						} catch (Exception exeption) {
@@ -57,70 +57,80 @@ public class SymCryptoMain {
 									"Couldn't decrypt " + file.getName() + ": " + exeption.getMessage(), "Error",
 									JOptionPane.ERROR_MESSAGE);
 						}
+						JOptionPane.showMessageDialog(frame, file.getName() + " Has Been Decrypted!",
+								"Encription Message", JOptionPane.INFORMATION_MESSAGE, new ImageIcon("src/check.png"));
+					} else if (response == JFileChooser.CANCEL_OPTION) {
+						JOptionPane.showMessageDialog(frame, "Canceled Decryption", "Cancel",
+								JOptionPane.INFORMATION_MESSAGE);
 					}
-					;
-					JOptionPane.showMessageDialog(frame, file.getName() + " Has been Decrypted", "Encription Message",
-							JOptionPane.INFORMATION_MESSAGE);
+
 				}
 
-			});
+			});			
 
-			//Images
-			ImageIcon encrypticon = new ImageIcon("Encrypt.png");
-			ImageIcon decrypticon = new ImageIcon("Decrypt.png");
-			
-			
 			// GUI PROPERTIES
 			Container pane = frame.getContentPane();
-			
+			frame.setResizable(false);
 			JPanel P = new JPanel();
-			P.setBackground(new Color(52, 73, 94));
+			P.setBackground(new Color(31, 71, 133));
+			
+			
 			JLabel welcome = new JLabel("Welcome to the CryptoJava");
 			JLabel protecc = new JLabel("Program Designed to protect your files");
 			JLabel select = new JLabel("I Want To:");
+			
+			//Images
+			ImageIcon icon1 = new ImageIcon("src/Encrypted.png");
+			ImageIcon icon2 = new ImageIcon("src/key.png");
+			JLabel home = new JLabel();
+			home.setIcon(new ImageIcon("src/home.png"));
 			
 			welcome.setFont(new Font("Monospaced", Font.BOLD, 26));
 			welcome.setForeground(Color.WHITE);
 			protecc.setFont(new Font("Monospaced", Font.PLAIN, 22));
 			protecc.setForeground(Color.WHITE);
+			select.setFont(new Font("Monospaced", Font.BOLD, 23));
+			select.setForeground(Color.WHITE);
 			btn1.setFont(new Font("Dialog", Font.PLAIN, 24));
 			btn2.setFont(new Font("Dialog", Font.PLAIN, 24));
+			
 
 			Dimension size = welcome.getPreferredSize();
 
 			// Upperlabels
-			welcome.setBounds(125, 50, size.width + 600, size.height);
-			protecc.setBounds(70, 80, size.width + 600, size.height);
-			select.setBounds(145, 280,size.width + 600, size.height);
+			welcome.setBounds(125, 40, size.width + 600, size.height);
+			protecc.setBounds(70, 70, size.width + 600, size.height);
+			select.setBounds(245, 250, size.width + 600, size.height);
+			home.setBounds(250, 120, 128, 128);
 
 			// Button Positions
-			btn1.setBounds(125, 300, 185, 65);
-			btn1.setIcon(encrypticon);
+			btn1.setBounds(65, 330, 240, 65);
 			btn1.setOpaque(false);
 			btn1.setForeground(Color.white);
+			btn1.setHorizontalTextPosition(JButton.RIGHT);
+			btn1.setIcon(icon1);
 			btn1.setBorder(new RoundedBorder(20));
 			btn1.setBackground(Color.white);
 
-			btn2.setBounds(350, 300, 200, 65);
-			btn2.setIcon(decrypticon);
+			btn2.setBounds(325, 330, 240, 65);
 			btn2.setOpaque(false);
+			btn2.setIcon(icon2);
 			btn2.setForeground(Color.white);
 			btn2.setBorder(new RoundedBorder(20));
 			btn2.setBackground(Color.white);
 
-
-			
 			P.setLayout(null);
 			P.add(welcome);
 			P.add(protecc);
+			P.add(select);
+			P.add(home);
+			
+			
 
 			P.add(btn1);
 			P.add(btn2);
 
 			pane.add(P);
-
-//	        pane.add(btn1);
-//	        pane.add(btn2);
 
 			frame.setSize(640, 480);
 			frame.setVisible(true);
@@ -130,30 +140,26 @@ public class SymCryptoMain {
 			System.err.println("Incorrect Algorithm Parameters: " + e.getMessage());
 		}
 
-	}
+	} 
 }
 
 class RoundedBorder implements Border {
 
-    private int radius;
+	private int radius;
 
+	RoundedBorder(int radius) {
+		this.radius = radius;
+	}
 
-    RoundedBorder(int radius) {
-        this.radius = radius;
-    }
+	public Insets getBorderInsets(Component c) {
+		return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+	}
 
+	public boolean isBorderOpaque() {
+		return true;
+	}
 
-    public Insets getBorderInsets(Component c) {
-        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
-    }
-
-
-    public boolean isBorderOpaque() {
-        return true;
-    }
-
-
-    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
-    }
+	public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+		g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+	}
 }
